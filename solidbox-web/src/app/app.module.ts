65bc5f2b7bcf95c36fe4cff2630a3f2f2f81a413
azharjk <azhr.project@gmail.com>
2022-04-23 17:28:36 +0700

    ui: add home page
    The home page is break down to some components
    which is card, widget, and the card header.

--- solidbox-web/src/app/app.module.ts
+++ solidbox-web/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PostCardComponent } from './post-card/post-card.component';
+import { CardHeadComponent } from './card-head/card-head.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostCardComponent,
+    CardHeadComponent
   ],
   imports: [
     BrowserModule,
