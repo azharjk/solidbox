e669e93e44aac33dbbbd2192a2affd6345ae6ba5
azharjk <azhr.project@gmail.com>
2022-04-27 05:44:33 +0700

    feat(web): registration validation before submitting

--- solidbox-web/src/app/register/register.component.spec.ts
+++ solidbox-web/src/app/register/register.component.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 import { RegisterComponent } from './register.component';
+import { UserService } from '../user/user.service';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let userService: UserService;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule, RouterTestingModule],
+      declarations: [RegisterComponent],
+    }).compileComponents();
+
+    userService = TestBed.inject(UserService);
+    router = TestBed.inject(Router);
+
+    const registerSpy = spyOn(userService, 'register');
+    registerSpy.and.returnValue(
+      of({
+        data: {
+          token: 'token123',
+        },
+      })
+    );
   });
 
   beforeEach(() => {
@@ -21,5 +40,33 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call UserService#register with given parameter', () => {
+    component.registerForm.controls['name'].setValue('Muhammad Azhari');
+    component.registerForm.controls['username'].setValue('azharazhar1010');
+    component.registerForm.controls['password'].setValue('helloguys1010');
+
+    component.onSubmit();
+
+    expect(userService.register).toHaveBeenCalledWith({
+      name: 'Muhammad Azhari',
+      username: 'azharazhar1010',
+      password: 'helloguys1010',
+    });
+  });
+
+  it('should store token to localStorage on submit', () => {
+    component.onSubmit();
+
+    expect(localStorage.getItem('token')).toBe('token123');
+  });
+
+  it('should navigate to [/] on submit', () => {
+    const navigateByUrlSpy = spyOn(router, 'navigateByUrl');
+
+    component.onSubmit();
+
+    expect(navigateByUrlSpy).toHaveBeenCalledWith('/');
   });
 });
--- solidbox-web/src/app/register/register.component.ts
+++ solidbox-web/src/app/register/register.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UserRegister } from '../user/user';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss', '../login/login.component.scss']
+  styleUrls: ['./register.component.scss', '../login/login.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  isInputInvalid(control: 'name' | 'username' | 'password') {
+    return (
+      this.registerForm.controls[control].invalid &&
+      this.registerForm.controls[control].touched
+    );
   }
 
+  onSubmit() {
+    const user = this.registerForm.value as UserRegister;
+    this.userService.register(user).subscribe(({ data }) => {
+      localStorage.setItem('token', data.token);
+      this.router.navigateByUrl('/');
+    });
+  }
 }
